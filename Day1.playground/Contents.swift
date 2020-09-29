916c6f255f3c8e10457348bb5e5ab6f97e50063f
Joel Moreno <joel32153@hotmail.com>
2020-09-29 12:39:03 -0500

    Day 1

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+//strings and integers
+var age = 38
+var population = 8_000_000
+//multi-line strings
+var str1 = """
+This goes
+over multiplate
+lines
+"""
+var str2 = """
+This goes \
+over multiplate \
+lines
+"""
+//Doubles and booleans
+var pi = 3.141
+var awsemome = true
+//String interpolation
+var score = 85
+var string = "Your score was \(score)"
+var results = "The test results are here: \(score)"
+//Constants
+let taylor = "swift"
+//Type annotations
+let strr = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
