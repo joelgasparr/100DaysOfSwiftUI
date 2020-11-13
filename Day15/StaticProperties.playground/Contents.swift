1e8debad450b317ad5fd72f579663ed3dc0740ff
Joel Moreno <joel32153@hotmail.com>
2020-11-12 20:34:21 -0600

    Static Properties

---
+++ Day15/StaticProperties.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+struct TaylorFan {
+    static var favoriteSong = "Look What You Made Me Do"
+    
+    var name: String
+    var age: Int
+}
+
+let fan = TaylorFan(name: "James", age: 25)
+print(TaylorFan.favoriteSong)
