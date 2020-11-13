c82a1a6b2020468eb2c239bb6cbe313f40353c53
Joel Moreno <joel32153@hotmail.com>
2020-11-12 20:06:09 -0600

    Structs

---
+++ Day14/Structs.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+struct Person {
+    var clothes: String
+    var shoes: String
+    
+    func describe() {
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
